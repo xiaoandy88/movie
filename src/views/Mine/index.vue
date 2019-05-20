9e862a9c3bdccec440bb1c3888ec18b6ee6edaf8
Andy <xiaoandy88@163.com>
2019-05-20 21:56:42 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="main">
+    <Header title="我的喵喵" />
+    <div id="content">
+      <Login />
+    </div>
+    <TabBar/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Login from '@/components/Login'
+import TabBar from '@/components/TabBar'
+export default {
+  name: 'Mine',
+  components: {
+    Header,
+    Login,
+    TabBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
